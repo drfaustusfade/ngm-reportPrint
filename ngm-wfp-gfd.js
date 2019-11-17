0573efad4d8fb231d53648920a12028ee6f23a3f
Patrick Fitzgerald <pfitzgerald@immap.org>
2019-11-18 01:34:04 +0600

    feature: added gfd print

--- ngm-wfp-gfd.js
+++ ngm-wfp-gfd.js
@@ -15,7 +15,7 @@
 		format: 'A4',
 		orientation: 'portrait',
 		margin: {
-			top: '1.5cm',
+			top: '1.4cm',
 			bottom: '1cm',
 			left: '1cm',
 			right: '1cm',
@@ -24,20 +24,18 @@
 			height: '1.2cm',
 			contents: phantom.callback( function() {
 				return '' +
-					'<h3 style="font-family: verdana, arial, sans-serif; font-size: 9px;">' + header_1 + '</h3>' +
-					'<h3 style="font-family: verdana, arial, sans-serif; font-size: 8px;">' + header_2 + '</h3>';
+					'<h3 style="font-family: verdana, arial, sans-serif; font-size: 10px; margin:0px;">' + header_1 + '</h3>' +
+					'<h3 style="font-family: verdana, arial, sans-serif; font-size: 9px; margin:0px 0px 10px 0px; color: #616161;">' + header_2 + '</h3>';
 			})
 		},
 		footer: {
 			height: '1cm',
 			contents: phantom.callback( function ( pageNum, numPages ) {
 				return '' +
-					'<div style="margin: 0 1cm 0 1cm; font-size: 0.65em">' +
-					'   <div style="font-family: verdana, arial, sans-serif; color: #888; padding:20px 20px 0 10px; border-top: 1px solid #ccc;">' +
-					'       <span>' + footer + '</span> ' +
-					'       <span style="float:right">' + pageNum + ' / ' + numPages + '</span>' +
-					'   </div>' +
-					'</div>';
+				'   <div style="font-family: verdana, arial, sans-serif; font-size: 9px; color: #888; padding:10px 10px 0 10px; border-top: 1px solid #ccc;">' +
+				'       <span>' + footer + '</span> ' +
+				'       <span style="float:right">' + pageNum + ' / ' + numPages + '</span>' +
+				'   </div>';
 			})
 		}
 	};
