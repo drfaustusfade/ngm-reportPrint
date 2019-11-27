72b8183dc77712424d3c1a6c574fb31d0ff79cf6
Patrick Fitzgerald <pfitzgerald@immap.org>
2019-11-28 00:26:26 +0600

    feature: removed header, added into html template script

--- ngm-wfp-gfd.js
+++ ngm-wfp-gfd.js
@@ -15,25 +15,25 @@
 		format: 'A4',
 		orientation: 'portrait',
 		margin: {
-			top: '1.4cm',
+			top: '1cm',
 			bottom: '1cm',
 			left: '1cm',
 			right: '1cm',
 		},
-		header: {
-			height: '1.2cm',
-			contents: phantom.callback( function() {
-				return '' +
-					'<h3 style="font-family: verdana, arial, sans-serif; font-size: 10px; margin:0px;">' + header_1 + '</h3>' +
-					'<h3 style="font-family: verdana, arial, sans-serif; font-size: 9px; margin:0px 0px 10px 0px; color: #616161;">' + header_2 + '</h3>';
-			})
-		},
+		// header: {
+		// 	height: '1.1cm',
+		// 	contents: phantom.callback( function() {
+		// 		return '' +
+		// 			'<h3 style="font-family: verdana, arial, sans-serif; font-size: 9px; margin:0px; font-weight: 300;">' + header_1 + '</h3>' +
+		// 			'<h3 style="font-family: verdana, arial, sans-serif; font-size: 8px; margin:0px 0px 10px 0px; color: #616161; font-weight: 300;">' + header_2 + '</h3>';
+		// 	})
+		// },
 		footer: {
 			height: '1cm',
 			contents: phantom.callback( function ( pageNum, numPages ) {
 				return '' +
-				'   <div style="font-family: verdana, arial, sans-serif; font-size: 9px; color: #888; padding:10px 10px 0 10px; border-top: 1px solid #ccc;">' +
-				'       <span>' + footer + '</span> ' +
+				'   <div style="font-family: verdana, arial, sans-serif; font-size: 8px; color: #888; padding:10px 10px 0 10px; border-top: 1px solid #ccc;">' +
+				'       <span style="float:left">' + footer + '</span> ' +
 				'       <span style="float:right">' + pageNum + ' / ' + numPages + '</span>' +
 				'   </div>';
 			})
@@ -47,7 +47,10 @@
 	page.content = fs.read( system.args[ 4 ] );
 
 	// output
-	window.setTimeout( function () {
+	// window.setTimeout( function () {
+	page.onLoadFinished = function() {
 		page.render( output, { format: 'pdf' });
 		phantom.exit();
-	}, 2000 );
+	};
+	// }, 2000 );
+
